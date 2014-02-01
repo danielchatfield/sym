2c4d09c9d8f06aab531d1907aa7bb023028333f8
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-02-01 17:33:58 +0000

    Fixed up to pass linting

--- cli.js
+++ cli.js
@@ -12,32 +12,32 @@ var optimist = require('optimist');
 var argv = optimist.argv;
 var conf = new Configstore('linky');
 
-var normalizeDestFolder = function(destFolder, src) {
-    if(!destFolder) {
+var normalizeDestFolder = function(src) {
+    if(src) {
         var dirs = src.split(path.sep);
-        if(dirs.length === 0) {
-            return '[source-folder]';
-        }
-        if(dirs[dirs.length-1] === '') {
-            return dirs[dirs.length-2];
-        } else {
-            return dirs[dirs.length-1];
+        if(dirs.length !== 0) {
+            if(dirs[dirs.length-1] === '') {
+                return dirs[dirs.length-2];
+            } else {
+                return dirs[dirs.length-1];
+            }
         }
     }
+
+    return '[source-folder]'; //No source set so we don't know what the folder will be called
 };
 
 var doLink = function() {
     var src = argv.src || conf.get('src');
     var dest = argv.dest || conf.get('dest');
 
-
-    var destFolder = conf.get('destFolder');
     if(!src) {
         var srcmsg = chalk.red('Source not set, run ') +
                   chalk.cyan('sym src') +
                   chalk.red(' first.');
         return console.error(srcmsg);
     }
+
     if(!dest) {
         var destmsg = chalk.red('Destination not set, run ') +
                   chalk.cyan('sym dest') +
@@ -45,20 +45,20 @@ var doLink = function() {
         return console.error(destmsg);
     }
 
-    destFolder = normalizeDestFolder(destFolder, src);
+    dest = path.join(dest, normalizeDestFolder(src));
 
-    linky(src, path.join(dest, destFolder));
+    linky(src, dest);
+
+    console.log(chalk.green('Link created'));
 };
 
 var setSource = function() {
-    conf.del('src');
     conf.set('src', process.cwd());
     var msg = chalk.cyan('Source set to: ') + chalk.cyan.bold(process.cwd());
     console.log(msg);
 };
 
 var setDest = function() {
-    conf.del('destFolder');
     conf.set('dest', process.cwd());
     var msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(process.cwd() + path.sep + '[src-folder]');
     console.log(msg);
@@ -75,20 +75,26 @@ if(notifier.update) {
 // it when I get a chance. It works for now.
 
 if(argv._.length === 0) {
-    var usage = 'Run ' + chalk.cyan('sym src') + ' in the source folder and ' +
-                chalk.cyan('sym dest') + ' in the destination folder then ' +
-                chalk.cyan('sym link.');
+    var padding = '      ';
+    var usage = padding + chalk.cyan('sym src') + '  - sets the current working directory as the source\n' +
+                padding + chalk.cyan('sym dest') + ' - sets the current working directory as the destination\n' +
+                padding + chalk.cyan('sym link') + ' - creates the symbolic link\n' +
+                padding + padding + chalk.white('Optionally specify the src and dest using the flags ') + chalk.cyan('-src') + ' and ' + chalk.cyan('-dest');
     console.log(usage);
+
+    console.log('\n\n\n');
 
     var src = conf.get('src');
     var dest = conf.get('dest');
-    var destFolder = normalizeDestFolder(conf.get('destFolder'), src);
 
-    var msg = chalk.cyan('Source set to: ') + chalk.cyan.bold(src);
-    console.log(msg);
+    if(src) {
+        console.log(padding + 'Source set to:\n' + padding + padding + chalk.cyan.bold(src));
+    }
 
-    msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(dest + path.sep + destFolder);
-    console.log(msg);
+    if(dest) {
+        var destFolder = normalizeDestFolder(src);
+        console.log(padding + 'Destination set to:\n' + padding + padding + chalk.cyan.bold(dest + path.sep + destFolder));
+    }
 
     return;
 }
@@ -99,17 +105,11 @@ if(command === 'src') {
     setSource();
 }
 
-if(command === '!src') {
-    setSource();
-    doLink();
-}
-
 if(command === 'dest') {
     setDest();
-    doLink();
 }
 
-if(command === 'link') {
+if(command === 'link' || argv.f) {
     doLink();
 }
 
