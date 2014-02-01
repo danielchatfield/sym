0c88705e28b1b9f7c50b2d6eb550f5b3b22c210d
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-02-01 17:00:17 +0000

    Refactored

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 'use strict';
+
 var chalk = require('chalk');
 var Configstore = require('configstore');
 var linky = require('./index.js');
@@ -25,6 +26,45 @@ var normalizeDestFolder = function(destFolder, src) {
     }
 };
 
+var doLink = function() {
+    var src = argv.src || conf.get('src');
+    var dest = argv.dest || conf.get('dest');
+
+
+    var destFolder = conf.get('destFolder');
+    if(!src) {
+        var srcmsg = chalk.red('Source not set, run ') +
+                  chalk.cyan('sym src') +
+                  chalk.red(' first.');
+        return console.error(srcmsg);
+    }
+    if(!dest) {
+        var destmsg = chalk.red('Destination not set, run ') +
+                  chalk.cyan('sym dest') +
+                  chalk.red(' first.');
+        return console.error(destmsg);
+    }
+
+    destFolder = normalizeDestFolder(destFolder, src);
+
+    linky(src, path.join(dest, destFolder));
+};
+
+var setSource = function() {
+    conf.del('src');
+    conf.set('src', process.cwd());
+    var msg = chalk.cyan('Source set to: ') + chalk.cyan.bold(process.cwd());
+    console.log(msg);
+};
+
+var setDest = function() {
+    conf.del('destFolder');
+    conf.set('dest', process.cwd());
+    var msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(process.cwd() + path.sep + '[src-folder]');
+    console.log(msg);
+};
+
+
 // Is there an update?
 if(notifier.update) {
     notifier.notify(true);
@@ -35,51 +75,41 @@ if(notifier.update) {
 // it when I get a chance. It works for now.
 
 if(argv._.length === 0) {
-    console.log(chalk.cyan('Usage coming soon...'));
+    var usage = 'Run ' + chalk.cyan('sym src') + ' in the source folder and ' +
+                chalk.cyan('sym dest') + ' in the destination folder then ' +
+                chalk.cyan('sym link.');
+    console.log(usage);
+
     var src = conf.get('src');
     var dest = conf.get('dest');
     var destFolder = normalizeDestFolder(conf.get('destFolder'), src);
+
     var msg = chalk.cyan('Source set to: ') + chalk.cyan.bold(src);
     console.log(msg);
-    var msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(dest + path.sep + destFolder);
+
+    msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(dest + path.sep + destFolder);
     console.log(msg);
+
     return;
 }
 
 var command = argv._[0];
 
 if(command === 'src') {
-    conf.set('src', process.cwd());
-    var msg = chalk.cyan('Source set to: ') + chalk.cyan.bold(process.cwd());
-    console.log(msg);
+    setSource();
+}
+
+if(command === '!src') {
+    setSource();
+    doLink();
 }
 
 if(command === 'dest') {
-    conf.del('destFolder');
-    conf.set('dest', process.cwd());
-    var msg = chalk.cyan('Destination set to: ') + chalk.cyan.bold(process.cwd() + path.sep + '[src-folder]');
-    console.log(msg);
+    setDest();
+    doLink();
 }
 
 if(command === 'link') {
-    var src = conf.get('src');
-    var dest = conf.get('dest');
-    var destFolder = conf.get('destFolder');
-    if(!src) {
-        var msg = chalk.red('Source not set, run ') +
-                  chalk.cyan('linky src') +
-                  chalk.red(' first.');
-        return console.error(msg);
-    }
-    if(!dest) {
-        var msg = chalk.red('Dest not set, run ') +
-                  chalk.cyan('linky dest') +
-                  chalk.red(' first.');
-        return console.error(msg);
-    }
-
-    destFolder = normalizeDestFolder(destFolder, src);
-    
-    linky(src, path.join(dest, destFolder));
+    doLink();
 }
 
